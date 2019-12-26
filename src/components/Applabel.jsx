01838761274a4569cd63f87c5a758e6d7eaaae5e
user6 <you@example.com>
2019-12-26 12:19:41 +0530

    component name added

--- src/components/Applabel.jsx
+++ src/components/Applabel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-export default class Test extends React.Component {
+export default class Applabel extends React.Component {
 
     render(){
         return (
