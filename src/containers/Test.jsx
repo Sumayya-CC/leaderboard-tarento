2baf286c52438264c42ff4dbdf8a74b2340b8b28
SonaArun <sonavs2013@gmail.com>
2019-12-26 12:54:17 +0530

    "ADD Score done"

--- src/containers/Test.jsx
+++ src/containers/Test.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import AddScore from '../components/AddScore'
 
 import TextField from '@material-ui/core/TextField';
 
 export default class Test extends React.Component {
 
     render(){
-        return (
-<Button variant="contained" style={{backgroundColor:"#33C6C3" , color:"white" , paddingLeft:50,paddingRight:50 }} >
-  save
-</Button>        )
+        return(
+        <AddScore/>    ) 
     }
 }
