7770922795ed5d63ebf6a717bb188521cd372e3c
FsherinP <fathimasherin362@gmail.com>
2019-12-26 12:18:25 +0530

    textfiels added

--- src/components/AppTextField.jsx
+++ src/components/AppTextField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
 
-export default class Test extends React.Component {
+export default class AppTextField extends React.Component {
 
     render(){
         return (
