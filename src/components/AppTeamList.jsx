41455f2ee573fd567b88e6a1036367a6094b3fc0
user6 <you@example.com>
2019-12-26 14:19:56 +0530

    add teamlist

--- src/components/AppTeamList.jsx
+++ src/components/AppTeamList.jsx
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-export default class Test extends React.Component {
+export default class AppTeamList extends React.Component {
 
     render() {
 
