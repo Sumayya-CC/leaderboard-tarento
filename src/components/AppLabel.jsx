45afb0e65c3b00d8183e552519cd5e217c301fec
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-12-26 14:22:44 +0530

    merge resolved

--- src/components/AppLabel.jsx
+++ src/components/AppLabel.jsx
@@ -5,13 +5,8 @@ export default class AppLabel extends React.Component {
 
     render(){
         return (
-<<<<<<< HEAD
-            <Typography variant="h5" gutterBottom style={{color: 'black'}}>
-            
-=======
             <Typography variant="h5" gutterBottom>
             {this.props.label}
->>>>>>> 71dc811e1b1f86ef0aae3e20b3a2d45f581b6e07
           </Typography>   
         )
     }
