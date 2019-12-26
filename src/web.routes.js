114623ed771e9918ac328416bef54b7097b2e362
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-12-26 11:32:27 +0530

    test component added

--- src/web.routes.js
+++ src/web.routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import Home from "./containers/Home";
+import Test from "./containers/Test";
 
 const Router = props => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/test" component={Test} />
     </Switch>
   </BrowserRouter>
 );
