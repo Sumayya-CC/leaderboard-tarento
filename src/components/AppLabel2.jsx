38798790238be429b82a3053e20d4d1f35a109b0
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-12-26 12:26:59 +0530

    AppLabel2 added

---
+++ src/components/AppLabel2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+export default class Applabel extends React.Component {
+
+    render(){
+        return (
+            <Typography>Anitta</Typography>
+        )
+    }
+}
