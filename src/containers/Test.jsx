1b8e8d8348c21f18a2d1927d8185558e3bc0110f
sreeragksgh <sreeragsajeesh@gmail.com>
2019-12-26 14:08:05 +0530

    FsherinP

--- src/containers/Test.jsx
+++ src/containers/Test.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import AppButton from '../components/AppButton';
+import AppLabel from '../components/AppLabel';
 
 import TextField from '@material-ui/core/TextField';
 
 export default class Test extends React.Component {
 
     render(){
-        return (
-<Button variant="contained" style={{backgroundColor:"#33C6C3" , color:"white" , paddingLeft:50,paddingRight:50 }} >
-  save
-</Button>        )
+        return(
+            <div>
+                <div>
+                    <AppLabel/>
+                </div>
+               
+                <div>
+                    <TextField/>
+                </div>
+                <div>
+                    <TextField/>
+                </div>
+                <div>
+                    <AppButton/>
+                </div>
+2
+            </div>
+        )
     }
 }
